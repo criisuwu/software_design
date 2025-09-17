4ecb010751280e792b088675f1a811bea5b5ee80
chomobon <crishomobonofer@gmail.com>
2025-09-17 13:27:52 +0200

    subo las carpetas

---
+++ teoria/main.cpp
@@ -0,0 +1,14 @@
+/* ---------------------------------
+File: main.cpp
+Author: Cristina Homobono Fernández
+Date: 17/09/2025
+Goal: The first try to C++ 
+------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world in C++!\n";
+    return 0;
+}
