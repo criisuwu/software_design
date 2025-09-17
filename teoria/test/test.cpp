a5f11b3d45843072bb178c9eb6db5f17f08b200f
chomobon <crishomobonofer@gmail.com>
2025-09-17 14:33:15 +0200

    Repasando las sentencias esenciales de la programacion

---
+++ teoria/test/test.cpp
@@ -0,0 +1,37 @@
+/* ---------------------------------
+File: test.cpp
+Author: Cristina Homobono Fernández
+Date: 17/09/2025
+Goal: Control sentences
+------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+    //bool flag = true;
+    int total = 0;
+    int n;
+
+    
+    // while(flag)
+    // {
+    //     std::cout << "introduce un numero: ";
+    //     std::cin >> n;
+    //     if(n < 0)
+    //         flag = false;
+    //     else
+    //         total = total + n;
+    // }
+    // do
+    // {
+    //     std::cout << "introduce un numero: ";
+    //     std::cin >> n;
+    //     if (n > 0)
+    //         total = total + n;
+    // }while(n > 0);
+    for (int i = 0; i < 5; i++)
+        
+    std::cout << "El total es: " << total << "\n";
+    return 0;
+}
