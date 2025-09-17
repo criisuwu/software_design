7721061dd96e1ba918eea66d280b13d162cf79e5
chomobon <crishomobonofer@gmail.com>
2025-09-17 13:51:53 +0200

    repaso de los .h y de la norma que quiere que sigamos a la hora de programas

---
+++ teoria/library.h
@@ -0,0 +1,4 @@
+// * Es principalmente una interfaz, declaras funciones que luego vas a usar.
+// * Es como un intermediario
+
+int getTotal(int a, int &b);
--- teoria/main.cpp
+++ teoria/main.cpp
@@ -6,15 +6,26 @@ Goal: The first try to C++
 ------------------------------------*/
 
 #include <iostream>
+#include "library.h"
+
+//la quiere sin _ y con una mayusc, lo mismo para las variables. Y con un verbo y un noun
+int getTotal(int a, int &b)
+{
+    b = 7; //no la duplica, si no la modifica en el hueco de memoria original
+    return a + b;
+}
+
 
 int main()
 {
-    int n;
+    int n = 5;
+    int n2 = 6;
     float d1 = 5.523;
-    char c = 'C';
+    //char c = 'C';
     std::cout << d1 << "\n";
     std::cout << "Introduce un numero: ";
     std::cin >> n;
     std::cout << "Tu numero es " << n << "\n";
+    std::cout << "La suma es: " << getTotal(n, n2) << "\n";
     return 0;
 }
